ebc8c39ee89c04fd3a13b7a816510acc72e08645
echewisi <josephechewisi@gmail.com>
2023-05-20 17:21:16 +0100

    rest config1

--- DRF_backend/settings.py
+++ DRF_backend/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'core',
     'blog_api',
+    'rest_framework'
 ]
 
 MIDDLEWARE = [
--- blog_api/views.py
+++ blog_api/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from core.models import Post
+from rest_framework.generics import ListAPIView, RetrieveDestroyAPIView
+from .serializers import PostSerializer
+class PostList(ListAPIView):
+    queryset= Post.postobjects.all() #returns all objects flagged as 'published'
+    serializer_class= PostSerializer
+class PostDetail(RetrieveDestroyAPIView):
+    pass
+
 
 # Create your views here.
