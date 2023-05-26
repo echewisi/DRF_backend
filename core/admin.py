748304914a72343425451f5f74ed2e7a72cc1058
echewisi <josephechewisi@gmail.com>
2023-05-26 03:39:09 +0100

    admin created

--- core/admin.py
+++ core/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
-
+from . import models
+@admin.register(models.Post)
+class AuthorAdmin(admin.ModelAdmin):
+    list_display=[
+        'title',
+        'id',
+        'status',
+        'slug',
+        'author'
+    ]
+    prepopulated_fields= {'slug': ('title',),}
 # Register your models here.
