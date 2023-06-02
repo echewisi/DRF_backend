647869bce700a428734c0f3cb6722ba3a29df610
echewisi <josephechewisi@gmail.com>
2023-06-02 03:22:14 +0100

    tests

--- blog_api/tests.py
+++ blog_api/tests.py
@@ -1,3 +1,12 @@
-from django.test import TestCase
+from rest_framework.test import APITestCase
+from django.urls import reverse
+from rest_framework import status
+from core.models import *
+from django.contrib.auth.models import User
+
+c
+
+
+
 
 # Create your tests here.
--- core/tests.py
+++ core/tests.py
@@ -2,10 +2,28 @@ from django.test import TestCase
 from django.contrib.auth.models import User
 from .models import Post, Category
 
+"""you can test the model with the test format below
+NB: you can edit it to match the requirements you want"""
 class Test_Create_Post(TestCase):
     @classmethod
     def setUpTestData(cls):
-        test_category= Category.objects.create(name= 'django')
-        test_user1= User.objects.create(username= 'testuser1', password= '123456')
+        test_category= Category.objects.create(name= 'study group')
+        test_user1= User.objects.create(username= 'patrick', password= '123456')
+        test_post= Post.objects.create(category_id=1, title='post title', excerpt= 'everything everywhere', content= 'null', slug='post-title', author_id=1, status= 'published')
+
+    def test_blog(self):
+        post= Post.postobjects.get(id=1)
+        category= Category.objects.get(id=1)
+        author= f'{post.author}'
+        excerpt= f'{post.excerpt}'
+        title= f'{post.title}'
+        content= f'{post.content}'
+        status= f'{post.status}'
+        self.assertEqual(author, 'patrick')
+        self.assertEqual(excerpt, 'everything everywhere')
+        self.assertEqual(title,'post title' )
+        self.assertEqual(status, 'published')
+        self.assertEqual(str(post), 'post title')
+        self.assertEqual(str(category), 'study group')
 
 # Create your tests here.
