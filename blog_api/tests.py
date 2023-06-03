b5a0eb67dd44981fba13fced8be7c2acf0fa8d0d
echewisi <josephechewisi@gmail.com>
2023-06-03 02:03:01 +0100

    api test

--- blog_api/tests.py
+++ blog_api/tests.py
@@ -4,9 +4,18 @@ from rest_framework import status
 from core.models import *
 from django.contrib.auth.models import User
 
-c
+class PostTests(APITestCase):
+    def test_view_posts(self):
+        
+        url= reverse('blog_api:list_create')
+        response= self.client.get(url, format= 'json')
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
 
-
-
-
+    def create_post(self):
+        self.test_category= Category.objects.create(name= 'django')
+        self.test_user1= User.objects.create_user(name= 'patrick', password= "123456")
+        data={"title": "new", "author": 1, "excerpt": "new", "content": "new"}  
+        url= reverse("blog_api:list_create")
+        response= self.client.post(url, data, format="json")
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
 # Create your tests here.
