1194683b3a63518402425d27642a594c0438fed9
echewisi <josephechewisi@gmail.com>
2023-06-13 00:14:30 +0100

    CORS config

--- DRF_backend/settings.py
+++ DRF_backend/settings.py
@@ -39,10 +39,12 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'core',
     'blog_api',
-    'rest_framework'
+    'rest_framework',
+    'corsheaders',
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -131,5 +133,8 @@ REST_FRAMEWORK={
         # 'rest_framework.permissions.IsAuthenticated'#if a non authenticated user tries to access this endpoint, a client-side error will be thrown
     
     ]
-    
 }
+
+CORS_ALLOWED_ORIGINS=[
+    'http://127.0.0.1:3000'
+]
