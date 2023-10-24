e36d7d9a271271932349b592b8ea2475ea7644f2
echewisi <josephechewisi@gmail.com>
2023-10-24 23:14:25 +0100

    changes

--- DRF_backend/settings.py
+++ DRF_backend/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
 from pathlib import Path
+from decouple import config
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-cj3avsr=+!%r=+4%)v+r5_7_t4ntx88fz-k4vr6y!vc-k5k5!a'
+SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -136,5 +137,5 @@ REST_FRAMEWORK={
 }
 
 CORS_ALLOWED_ORIGINS=[
-    'http://127.0.0.1:3000'
+    'http://localhost:3000'
 ]
